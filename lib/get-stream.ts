af7c865c30124f00d8fbcadcd9a61c02fb7d2b5b
Olegzandr VD <alex@oresoftware.com>
2017-11-10 14:26:55 -0800

    pdev:set

--- lib/get-stream.ts
+++ lib/get-stream.ts
@@ -3,6 +3,7 @@
 //core
 import {Stream, Transform} from "stream";
 import util = require('util');
+import assert = require('assert');
 
 //npm
 import parser from 'tap-json-parser';
@@ -50,10 +51,14 @@ export const getJSONStdioStream = function () {
 
   p = jsonStdio.createParser();
 
-  p.on(jsonStdio.stdEventName, function (obj: Object) {
+  let stdEventName = jsonStdio.stdEventName;
+  assert(typeof stdEventName === 'string',
+    `Suman implementation error: 'json-stdio' library does not export expected property.`);
+
+  p.on(stdEventName, function (obj: Object) {
 
     if (!obj) {
-      log.warning('implementation warning: no jsonStdio object passed.');
+      log.warning(`Suman implementation warning: no json-stdio object passed to '${stdEventName}' handler.`);
       return;
     }
 
@@ -63,8 +68,8 @@ export const getJSONStdioStream = function () {
       tb.emit(msgType, obj);
     }
     else {
-      log.warning('json stdio object messageType was not a key in suman-events.');
-      log.warning('Object value:', util.inspect(obj));
+      log.warning(' => json stdio object "messageType" property was not a key in suman-events.');
+      log.warning(' => json stido object value:', util.inspect(obj));
     }
 
   });
